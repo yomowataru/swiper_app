38be2c3fb7eef31f405d36b71a62e9caad5481c4
EC2 Default User <yomoyomo0503@gmail.com>
2023-10-20 13:32:04 +0000

    S,jQuery実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
+import "script.js"
+import Swiper from 'swiper/swiper-bundle.js';
+import 'swiper/swiper-bundle.css';　
+
 
 Rails.start()
 Turbolinks.start()
